0ad3c41aa2e8a25b4e283ee1080901e68140a746
Ken Barlow <kenbrlow@gmail.com>
2021-02-21 15:49:24 -0700

    commit tree bcc1aba558122d226a124ada47b766a3740aa784

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    var arrayMessages = [];
+    for (let t = 0; t < names.length; t++) {
+        const customMessage = `Thank you, ${names[t]}, for the wonderful surprise gift!`;
+        arrayMessages.push(customMessage);
+    }
+    return arrayMessages;
+}
+
+function countDown(numberToStartAt) {
+    while (numberToStartAt > -1) {
+        console.log(numberToStartAt--);
+    }
+}
